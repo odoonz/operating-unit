c227ff6d5812d0901a9e1f3b5aecbcfbc4c4e91d
Graeme Gellatly <gdgellatly@gmail.com>
2017-11-23 22:58:39 +1300

    [LICENSE] Correct to LGPL

--- stock_account_operating_unit/__manifest__.py
+++ stock_account_operating_unit/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Open For Small Business Ltd
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Stock Account Operating Unit',
     'summary': """
         Adds more sophisticated stock accounting to operating units""",
     'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'author': 'Open For Small Business Ltd,Odoo Community Association (OCA)',
     'website': 'https://o4sb.com',
     'depends': ['stock_account', 'sale_stock', 'operating_unit'
