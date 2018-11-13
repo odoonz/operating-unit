0b4fd593b5c1f763bfb48bd21c7577bb33cc2241
Graeme Gellatly <gdgellatly@gmail.com>
2018-11-13 17:22:50 +1300

    Fix sale_report

--- sale_operating_unit/report/sale_report.py
+++ sale_operating_unit/report/sale_report.py
@@ -5,21 +5,15 @@ from odoo import fields, models
 
 
 class SaleReport(models.Model):
-
     _inherit = "sale.report"
 
-    operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit')
+    operating_unit_id = fields.Many2one("operating.unit", "Operating Unit", readonly=True)
 
-    def _select(self):
-        select_str = super(SaleReport, self)._select()
-        select_str += """
-            ,s.operating_unit_id
-        """
-        return select_str
-
-    def _group_by(self):
-        group_by_str = super(SaleReport, self)._group_by()
-        group_by_str += """
-            ,s.operating_unit_id
-        """
-        return group_by_str
+    def _query(self, with_clause="", fields={}, groupby="", from_clause=""):
+        fields[
+            "operating_unit_id"
+        ] = ", s.operating_unit_id as operating_unit_id"
+        groupby += ", s.operating_unit_id"
+        return super(SaleReport, self)._query(
+            with_clause, fields, groupby, from_clause
+        )
