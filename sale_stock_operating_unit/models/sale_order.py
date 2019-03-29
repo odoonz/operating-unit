6aa07a926760cad761c45e472f4f0eaab7664dda
Graeme Gellatly <gdgellatly@gmail.com>
2019-03-29 13:05:54 +1300

    Revert OU default warehouse change as causing issues

--- sale_stock_operating_unit/models/sale_order.py
+++ sale_stock_operating_unit/models/sale_order.py
@@ -2,7 +2,7 @@
 # Jordi Ballester Alomar
 # © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo import api, models, fields
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
@@ -13,17 +13,3 @@ class SaleOrder(models.Model):
         if (self.operating_unit_id and
                 self.operating_unit_id not in self.user_id.operating_unit_ids):
             self.user_id = False
-
-    @api.model
-    def _default_warehouse_id(self):
-        company = self.env.user.company_id.id
-        domain = [('company_id', '=', company)]
-        operating_unit = self.env.user.default_operating_unit_id
-        if operating_unit and company == operating_unit.company_id.id:
-            domain.append(('operating_unit_id', '=', operating_unit.id))
-        warehouse_ids = self.env['stock.warehouse'].search(domain, limit=1)
-        return warehouse_ids
-
-    warehouse_id = fields.Many2one(default=_default_warehouse_id)
-
-
